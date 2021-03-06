5cf0bd6e7f1b487ab5ac4b70fbe711fe6c99ea01
雨飞 <51397552+yufeifeiyu@users.noreply.github.com>
2021-03-06 14:54:26 +0800

    Update DataMark.m

--- DataMark.m
+++ DataMark.m
@@ -10,17 +10,19 @@
 function  DataMark(fname,image_name)
 % description :This function takes the generated json file and the original image as input data, and generates the image data label, with land as 0 and water as 1.
 
+
 addpath('jsonlab\jsonlab'); %jsonlab库文件存放路径
 jsonData=loadjson(fname);
 [m,n,k]=size(imread(image_name));
+
 m=ceil(m/16)*16;
 n=ceil(n/16)*16;
 
 %根据labelme的划分生成划分图像
 I=zeros(m,n);
 label=I;
-% subplot(1,2,1),imshow(label),title('original');
-[i,j]=size(jsonData.shapes);
+
+[~,j]=size(jsonData.shapes);
 for i=1:j
     c = jsonData.shapes{1, i}.points(:,1);
     r =  jsonData.shapes{1, i}.points(:,2);
@@ -29,18 +31,8 @@ for i=1:j
 end
 
 %调整图像的大小
-if m<1024
-    m=1024;
-elseif m>4096
-    m=4096;
-end
-if n<1024
-    n=1024;
-elseif n>4096
-    n=4096;
-end
+m=1024;n=1024;
 label=imresize(label,[m,n]);
 
-% subplot(1,2,2),imshow(label),title('Water display');
-save('image_label.mat','label','image_name','m','n','k') 
+save('image_label.mat','label','image_name','m','n','k')
 end
